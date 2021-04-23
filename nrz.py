a2994bf9ccc38a3be7d004d86395ea9fe7ab8b2a
Max Braga <cbcc.max@gmail.com>
2021-04-22 21:47:35 -0300

    Update nrz.py

--- nrz.py
+++ nrz.py
@@ -1,15 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+plt.title('NRZ')
+plt.xlabel('Largura')
+plt.ylabel('Bits')
 def nrz(vetor):
 	for i in range(0,len(vetor)):
 		plt.plot([i,i+1], [vetor[i],vetor[i]], 'yo-', color='k')
 		if i != 0:
 			if vetor[i-1] != vetor[i]:
 				plt.plot([i,i], [0,1], 'yo-', color='k')
-
-
+                
 #NonReturnZero
 
 binario = [1,0,1,1,0]
