aeeb57b77459387d11d2423d9b06dbe4e9190ea8
Max Braga <cbcc.max@gmail.com>
2021-04-22 21:26:28 -0300

    Create manchesterDiferencial.py
    adicionando os codigos do Man Dif

---
+++ manchesterDiferencial.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def manchester_differential(vetor):
+	pos = -1
+	for i in range(0,len(vetor)):
+		if vetor[i] == 1:
+			pos = (-1 if pos == 1 else 1)
+		else:
+			plt.plot([i,i], [1,-1], 'yo-', color='k')
+		if pos == 1:
+			if i != 0:
+				plt.plot([i,i+0.5], [1,1], 'yo-', color='k')
+			plt.plot([i+0.5,i+0.5], [1,-1], 'yo-', color='k')
+			plt.plot([i+0.5,i+1], [-1,-1], 'yo-', color='k')
+		else:
+			if i != 0:
+				plt.plot([i,i+0.5], [-1,-1], 'yo-', color='k')
+			plt.plot([i+0.5,i+0.5], [1,-1], 'yo-', color='k')
+			plt.plot([i+0.5,i+1], [1,1], 'yo-', color='k')
+	plt.plot([0,0.5], [-1,-1], color='k')
+
+#ManchesterDiferencial
+binario = [0,1,0,0,1,1]
+manchester_differential(binario)
+plt.show()
